acf48fc5d2464a9ac0ac1ff00e47a32c30f7a29a
SwiftMan <swift@nhnent.com>
2018-11-20 21:04:34 +0900

    first commit

---
+++ HelloRx/HelloRx/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloRx
+//
+//  Created by SwiftMan on 20/11/2018.
+//  Copyright © 2018 SwiftMan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
